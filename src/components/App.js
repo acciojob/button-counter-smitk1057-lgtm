4dbea4f32c9ac7a99aa65f5fc43f89aa4e8a51a7
Sumit Kumar <smitk1057@gmail.com>
2026-04-19 10:00:47 +0530

    Counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
+import React, { useState } from "react"
 
-import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCounter]=useState(0)
+  console.log("count",count)
   return (
     <div>
         {/* Do not remove the main div */}
+        
+       <p>Button clicked {count} times</p>
+       <button onClick={()=>setCounter((count)=>count+1)}>Click Me</button>
+
     </div>
   )
 }
